fa4e19282db9fdf6825601b4b5fe05e4251df798
Yoshiki Aoki <ringohub@gmail.com>
2018-09-18 01:27:53 +0900

    [IMPROVEMENT] Add config checker to index.js

--- index.js
+++ index.js
@@ -2,14 +2,42 @@
 
 const Botkit = require('botkit');
 const chalk = require('chalk');
+const packageInfo = require('./package');
 
 const fs = require('fs');
 const path = require('path');
+const checkEnv = require('check-env');
 
 process.on('unhandledRejection', console.dir);
 
+checkEnv([
+  'AB_SLACK_API_TOKEN',
+  'HUBOT_CONFLUENCE_USER',
+  'HUBOT_CONFLUENCE_USER',
+  'HUBOT_CONFLUENCE_USER',
+  'HUBOT_JIRA_USER',
+  'HUBOT_JIRA_PASSWORD',
+  'HUBOT_JIRA_URL'
+]);
+
+const config = {
+  "hello": {
+    "version": packageInfo.version
+  },
+  "confluencePageTitle": {
+    "user":  process.env.HUBOT_CONFLUENCE_USER,
+    "password": process.env.HUBOT_CONFLUENCE_PASSWORD,
+    "baseUrl": process.env.HUBOT_CONFLUENCE_URL
+  },
+  "jiraTicketTitle": {
+    "user":  process.env.HUBOT_JIRA_USER,
+    "password": process.env.HUBOT_JIRA_PASSWORD,
+    "baseUrl": process.env.HUBOT_JIRA_URL
+  }
+};
+
 const controller = Botkit.slackbot({
-  debug: process.env.NODE_DEBUG || false
+  debug: process.env.NODE_DEBUG ? process.env.NODE_DEBUG.split(',').includes('botkit') : undefined
 });
 
 if (!process.env.AB_SLACK_API_TOKEN) {
@@ -32,6 +60,6 @@ fs.readdir(plugins, (err, list) => {
   for (const file of list) {
     const plugin = path.resolve(plugins, file);
     console.log(chalk.green(`Load ${file}`));
-    require(plugin)(controller);
+    require(plugin)(controller, config);
   }
 });
