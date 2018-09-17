a78650dd58327429de39e102d0ed053c06b2b564
Yoshiki Aoki <ringohub@gmail.com>
2018-09-18 01:25:36 +0900

    [FEATURE] Add version info in hello module

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 FROM node:carbon-alpine
 
+RUN npm i npm@latest -g
 WORKDIR /usr/src/app
 COPY package*.json ./
 COPY plugins ./plugins
 COPY index.js ./
-RUN npm install --only=production
+RUN npm install --only=production && npm audit fix
 CMD [ "npm", "start" ]
--- plugins/hello.js
+++ plugins/hello.js
@@ -1,7 +1,10 @@
 'use strict';
 
-module.exports = controller => {
+module.exports = (controller, option) => {
+  const config = option.hello;
+  const version = config.version;
+
   controller.hears('hello', ['direct_message', 'direct_mention', 'mention'], (bot, msg) => {
-    bot.reply(msg, 'hello :wave:');
+    bot.reply(msg, `hello :wave: ${version}`);
   });
 };
