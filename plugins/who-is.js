8be5715327de9f0bcf8f4180e53ed9f64973829d
Yoshiki Aoki <ringohub@gmail.com>
2018-09-18 01:26:20 +0900

    [FEATURE] Add channel url function into who-is module

--- plugins/who-is.js
+++ plugins/who-is.js
@@ -3,11 +3,12 @@
 const p = require('util').promisify;
 
 module.exports = controller => {
-  controller.hears('who is @?(.+)', ['direct_message', 'direct_mention'], async (bot, msg) => {
+
+  controller.hears('whois [@!]?(.+)', ['direct_message', 'direct_mention'], async (bot, msg) => {
     const name = msg.match[1];
     const user = await (async (name) => {
-      if (/<@(.+)>/.test(name)) {
-        const uid = name.match(/<@(.+)>/)[1];
+      if (/<[@!](.+)>/.test(name)) {
+        const uid = name.match(/<[@!](.+)>/)[1];
         return await p(bot.api.users.info)({user: uid});
       } else {
         return (await p(bot.api.users.list)({})).members.find(m => m.name === name);
@@ -16,4 +17,10 @@ module.exports = controller => {
 
     bot.reply(msg, '```' + JSON.stringify(user, null, 2) + '```');
   });
+
+  controller.hears('chinfo #?(.+)', ['direct_message', 'direct_mention', 'ambient'], async (bot, msg) => {
+    const team = msg.team;
+    const name = msg.match[1].match(/<#(.+)\|.+>/)[1];
+    bot.reply(msg, `slack://channel?team=${team}&id=${name}`)
+  })
 };
